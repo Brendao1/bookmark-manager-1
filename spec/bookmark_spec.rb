5b95ca0056f201d02fd859ad44c2b05ca63f860e
“Brenda” <odonnell.brenda@gmail.com>
2019-02-27 17:16:25 +0000

    Added database_helpers for persistent data

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,7 +1,5 @@
 require 'bookmark'
-
-
-# require 'database_helpers'
+require 'database_helpers'
 
 describe Bookmark do
   describe '.all' do
@@ -13,12 +11,11 @@ describe Bookmark do
       Bookmark.create(url: "http://www.github.com", title: "Github")
       Bookmark.create(url: "http://www.google.com", title: "Google")
 
-
       bookmarks = Bookmark.all
 
       expect(bookmarks.length).to eq 3
       expect(bookmarks.first).to be_a Bookmark
-      expect(bookmarks.first.id).to eq bookmark.first['id']
+      expect(bookmarks.first.id).to eq bookmark.id
       expect(bookmarks.first.title).to eq 'Makers Academy'
       expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
       end
@@ -27,14 +24,13 @@ describe Bookmark do
   describe '.create' do
     it 'creates a new bookmark' do
       bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark').first
-      persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
+      # removed .first
+      # bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark').first
 
-      # expect(bookmark['url']).to eq 'http://www.testbookmark.com'
-      # expect(bookmark['title']).to eq 'Test Bookmark'
-
-
+      persisted_data = persisted_data(id: bookmark.id)
       expect(bookmark).to be_a Bookmark
-      expect(bookmark.id).to eq persisted_data.first['id']
+      expect(bookmark.id).to eq persisted_data.id
+      # removed .first['id']
       expect(bookmark.title).to eq 'Test Bookmark'
       expect(bookmark.url).to eq 'http://www.testbookmark.com'
     end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,26 +1,3 @@
-# feature "Index page for the bookmarks" do
-#   scenario "index page for the bookmarks" do
-#     visit '/'
-#     expect(page).to have_content "Bookmark Manager!"
-#   end
-# end
-
-# feature "Viewing bookmarks" do
-#   scenario "a user can see bookmarks" do
-#
-#     # Add the test data
-#     Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
-#     Bookmark.create(url: 'http://www.github.com', title: 'Github')
-#     Bookmark.create(url: 'http://www.google.com', title: 'Google')
-#
-#     visit '/bookmarks'
-#
-#     expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
-#     expect(page).to have_link('Github', href: 'http://www.github.com')
-#     expect(page).to have_link("Google", href: 'http://www.google.com')
-#   end
-# end
-
 feature "viewing bookmarks in /bookmarks route" do
   scenario "show list of bookmarks" do
 
